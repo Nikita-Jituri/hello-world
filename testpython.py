9ab40fbb1626c2b722219d686ee2ce0e1769a245
Nikita Jituri <nikita.jituri@walmart.com>
2023-01-22 14:52:37 +0530

    Update testpython.py
    commit python logic

--- testpython.py
+++ testpython.py
@@ -3,7 +3,8 @@ from github import Github
 print("hello world - inside python script")
 
 g = Github(base_url="https://github.com/api/v3", login_or_token="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")
-repo = g.get_user().get_repo("helllo-world")
+
+repo = g.get_user().get_repo("hello-world")
 print(repo.full_name)
 contents = repo.get_contents("README.md", ref="main")
 print(contents)
